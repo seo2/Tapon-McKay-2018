0d322b21a46f26f2a77c13d49d77b9ba1cd095a8
Cristian <seo2@seo2.cl>
2018-06-14 11:55:39 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Galletas McKay</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://www.mckay.cl/assets/css/tapon.css" >
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+<style id="antiClickjack">body{display:none !important;}</style>
+<script type="text/javascript">
+   if (self === top) {
+       var antiClickjack = document.getElementById("antiClickjack");
+       antiClickjack.parentNode.removeChild(antiClickjack);
+   } else {
+       top.location = self.location;
+   }
+</script>
+
+<script>
+
+!function(f,b,e,v,n,t,s)
+
+{if(f.fbq)return;n=3Df.fbq=3Dfunction(){n.callMethod?
+
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+
+if(!f._fbq)f._fbq=3Dn;n.push=3Dn;n.loaded=3D!0;n.version=3D'2.0';
+
+n.queue=3D[];t=3Db.createElement(e);t.async=3D!0;
+
+t.src=3Dv;s=3Db.getElementsByTagName(e)[0];
+
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+
+'https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '129215334312197');
+
+fbq('track', 'PageView');
+
+</script>
+<noscript>
+	<img height="1" width="1" src="https://www.facebook.com/tr?id=129215334312197&ev=PageView&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
+
+    
+  </head>
+  <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQLDJF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->	  
+	  
+	<header>
+		<div class="container">
+			<div class="row hidden-xs">
+				<div class="col-md-2 col-md-offset-1">
+					<img src="assets/img/logo-mckay.png" class="img-responsive">
+				</div>
+			</div>
+			<div class="row visible-xs">
+				<div class="col-xs-6 col-xs-offset-3">
+					<img src="assets/img/logo-mckay.png" class="img-responsive">
+				</div>
+			</div>
+		</div>
+	</header>
+	<section id="tapon">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<img src="assets/img/txt-tapon.png" class="img-responsive">
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer>
+	    <div class="container">
+		    <div class="row">
+		          <ul id="nav_footer" class="pull-right">
+		            <li><a target="_blank" href="assets/descargas/Terminos_y_Condiciones_de_Uso_-_Sitios_web_de_Nestle_Chile_SA.pdf">condiciones</a> </li>
+		            <li><a target="_blank" href="assets/descargas/Aviso_de_Privacidad_Nestle_Chile_SA.pdf">privacidad</a> </li>
+		            <li><a target="_blank" href="assets/descargas/Politica_de_Nestle_sobre_Cookies_Nestle_Chile_SA.pdf">cookies</a> </li>
+		          </ul>
+		    </div>
+	    </div>
+	</footer>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-80983481-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="assets/js/tapon.js"></script>
+  </body>
+</html>
